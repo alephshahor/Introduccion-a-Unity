d42ffdc417365d2f4e58613e83c221df80726af0
Adrián Álvarez <adrianalvarezleon@gmail.com>
2019-10-06 06:12:28 -0700

    Escena completada

---
+++ Assets/Orbit.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Orbit : MonoBehaviour
+{
+    public Vector3 transformation;
+    public float velocity;
+    private Vector3 initialPosition;
+
+    void Start()
+    {
+        initialPosition = transform.position;
+    }
+
+    void Update()
+    {
+        transform.RotateAround(initialPosition + transformation, Vector3.up, velocity * Time.deltaTime);
+    }
+}
+
+
+
+
